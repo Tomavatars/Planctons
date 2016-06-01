5386c23c8d54e46afe1f9e2530e2c7851709666b
Tom Avatars <t.o.m.avatars@gmail.com>
2016-06-01 17:36:27 +0200

    Go Go

---
+++ Source/Humanoplancton/MyClass.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Humanoplancton.h"
+#include "MyClass.h"
+
+MyClass::MyClass()
+{
+}
+
+MyClass::~MyClass()
+{
+}
